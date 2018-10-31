0403c7c80fedcc5eaf33247be5b4f3966d81261a
alex <alexsamail@mail.ru>
2018-11-01 00:14:12 +0300

    Third part.

---
+++ TestLevel/Assets/MouseLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	[HideInInspector]
+	public float xRotation;
+	[HideInInspector]
+	public float yRotation;
+	public float lookSentivity = 5;
+	[HideInInspector]
+	public float currXRotation;
+	[HideInInspector]
+	public float currYRotation;
+	[HideInInspector]
+	public float xRotationVelocity;
+	[HideInInspector]
+	public float yRotationVelocity;
+	public float smoothDampTime = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		xRotation -= Input.GetAxis ("Mouse Y") * lookSentivity;
+		yRotation += Input.GetAxis ("Mouse X") * lookSentivity;
+
+		xRotation = Mathf.Clamp (xRotation, -90, 90); 
+
+		currXRotation = Mathf.SmoothDamp (currXRotation, xRotation, ref xRotationVelocity, smoothDampTime);
+		currYRotation = Mathf.SmoothDamp (currYRotation, yRotation, ref yRotationVelocity, smoothDampTime);
+
+
+		transform.rotation = Quaternion.Euler (currXRotation, currYRotation, 0);
+	}
+}
